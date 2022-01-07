a080ade6ad754750031d9cb7e63213ab8853da2a
Thomas Hesse <thomas.hesse@neoloan.de>
2022-01-07 20:32:04 +0100

    refactor: add root logger to redditbot

--- redditbot.py
+++ redditbot.py
@@ -16,6 +16,7 @@ from config import REDDIT_PASSWORD
 from config import REDDIT_USERNAME
 from config import USER_AGENT
 from src.gif_utilities import cut_gif as cut_gif_func
+from src.logger import root
 
 
 def run(reddit, imgur):
@@ -24,19 +25,19 @@ def run(reddit, imgur):
     params = {}
     inbox = reddit.inbox.unread(limit=None)
     for m in inbox:
-        print(f'Received message: {m.body}')
+        root.info(f'Received message: {m.body}')
         # todo queue and periodically work through queue
         # extract params
         matches = pattern.search(m.body)
         if matches is None:
-            print('Skipping message because no match was found.')
+            root.warn('Skipping message because no match was found.')
             m.mark_read()
             continue
-        print(f'Found pattern matches: {matches.groups()}')
+        root.debug(f'Found pattern matches: {matches.groups()}')
         params['start'] = int(matches.group(2))
         params['end'] = int(matches.group(4))
         params['img'] = Image.open(requests.get(m.submission.url, stream=True).raw)
-        print(params)
+        # print(params)
         cut_gif, target_duration = cut_gif_func(**params)
 
         with NamedTemporaryFile(mode='wb', suffix='.gif') as gif:
@@ -48,12 +49,17 @@ def run(reddit, imgur):
                 duration=target_duration,
                 loop=0
             )
-            print('Uploading to imgur...')
+            root.info('Uploading to imgur...')
             res = imgur.upload_from_path(gif.name, anon=False)
-            print('Upload finished!')
+            root.info('Upload finished!')
         # reply with link to the just cut gif and mark as unread
-        m.reply(f'Here is your cut GIF: {res.get("link")}')
+        issue_link = f'https://www.reddit.com/message/compose/?to=domac&subject={REDDIT_USERNAME}%20issue&message=' \
+                     f'Add a link to the gif or comment in your message%2C I%27m not always sure which request is ' \
+                     f'being reported. Thanks for helping me out! '
+        bot_footer = f"---\n\n^(I am a bot.) [^(Report an issue)]({issue_link})"
+        m.reply(f'Here is your cut GIF: {res.get("link")}\n{bot_footer}')
         m.mark_read()  # done
+        root.info('Reddit reply sent!')
 
 
 # Credits: https://stackoverflow.com/a/22702362/2402281
@@ -83,6 +89,7 @@ class IntervalTimer(StoppableThread):
 
 
 if __name__ == '__main__':
+    root.info('Starting gifCutterBot...')
     reddit_instance = praw.Reddit(
         user_agent=USER_AGENT,
         client_id=REDDIT_CLIENT_ID,
@@ -90,11 +97,16 @@ if __name__ == '__main__':
         username=REDDIT_USERNAME,
         password=REDDIT_PASSWORD,
     )
+    root.info('Reddit client connected!')
     imgur_instance = ImgurClient(IMGUR_CLIENT_ID, IMGUR_CLIENT_SECRET)
+    root.info('Imgur client connected!')
 
     # run script every 10 seconds
-    it = IntervalTimer(10, lambda: run(reddit=reddit_instance, imgur=imgur_instance))
+    interval_seconds = 10
+    it = IntervalTimer(interval=interval_seconds, worker_func=lambda: run(reddit=reddit_instance, imgur=imgur_instance))
     try:
+        root.info(f'Running every {interval_seconds} seconds.')
         it.start()  # run indefinitely
     except (InterruptedError, KeyboardInterrupt):
+        root.info('Periodic execution stopped.')
         it.stop()  # stop on ctrl+c or sigterm
