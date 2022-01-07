4fda512a58cd737b9a0eb5ab7d131457973b5029
Thomas Hesse <thomas.hesse@neoloan.de>
2022-01-07 20:31:33 +0100

    feat: load version from central file

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ REDDIT_PASSWORD = getenv('REDDIT_PASSWORD')
 REDDIT_CLIENT_ID = getenv('REDDIT_CLIENT_ID')
 REDDIT_CLIENT_SECRET = getenv('REDDIT_CLIENT_SECRET')
 
-USER_AGENT = 'web:gifcutter:v1.0.0 (by /u/domac)'
+with open('VERSION', 'r') as f:
+    USER_AGENT = f'web:gifcutterbot:v{f.readline()} (by /u/domac)'
 
 IMGUR_CLIENT_ID = getenv('IMGUR_CLIENT_ID')
 IMGUR_CLIENT_SECRET = getenv('IMGUR_CLIENT_SECRET')
