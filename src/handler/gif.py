172034bb250e74b0e4376733609db2d4bda18ba0
Thomas Hesse <mail@thomas-hesse.eu>
2022-03-28 21:30:51 +0200

    fix: gif handler cut operation

--- src/handler/gif.py
+++ src/handler/gif.py
@@ -3,43 +3,71 @@ from __future__ import annotations
 import math
 from io import BytesIO
 from typing import List
-from typing import Tuple
 
-from PIL import Image, ImageSequence
+import PIL
+import PIL.Image
+import PIL.ImageSequence
+import PIL.GifImagePlugin
 
+import src.model.result as result
 from src import gif_utilities
 from src.execution import task
 from src.handler import base
-from src.util import decorator
-from src.util.aux import fix_start_end_swap
+from src.model.media_type import MediaType
 
 
 class GifCutHandler(base.BaseCutHandler):
     # @decorator.create_hook(pre=None, post=base.post_cut_hook)
-    def cut(self, stream: BytesIO, config: task.TaskConfig) -> Tuple[List[Image], float]:
+    def cut(self, stream: BytesIO, config: task.TaskConfig) -> result.Result:
         start_ms = config.start
         end_ms = config.end
         watermark = config.watermark
         duration = config.duration
-        ext = config.extension
 
         stream.seek(0)
-        image: Image = Image.open(stream)
+        image: PIL.Image = PIL.Image.open(stream)
         if duration is None:
             duration = gif_utilities.get_gif_duration(image=image)
-
-        start_ms, end_ms = fix_start_end_swap(start=start_ms, end=end_ms)
-        start_ms = max(start_ms, 0)  # put a realistic lower bound on end
-        duration_ms = duration * 1000
-        end_ms = min(end_ms or math.inf, duration_ms)  # put a realistic upper bound on end
+            end_ms = min(end_ms or math.inf, duration * 1000)  # put a realistic upper bound on end
+        # duration_ms = duration * 1000
         target_duration_ms = end_ms - start_ms
-
-        # multiply duration by 1000 to have it in ms rather than s
-        assert 0 < target_duration_ms < duration_ms and end_ms <= duration_ms  # sanity check
-
-        frames_out: List[Image.Image] = []
-        for frame in ImageSequence.Iterator(image):
-            frame = frame.convert('RGB')
-            frames_out.append(watermark(frame))
-        assert len(frames_out) > 0
-        return frames_out, target_duration_ms / (1000 * len(frames_out))
+        # assert 0 < target_duration_ms < duration_ms and end_ms <= duration_ms  # sanity check
+        # iterate GIF frames and optionally apply watermark
+        frames: List[PIL.Image.Image] = []
+        cum_duration_ms = 0
+        frame: PIL.Image.Image
+        for frame in PIL.ImageSequence.Iterator(image):
+            # https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#gif
+            if cum_duration_ms < start_ms:
+                cum_duration_ms += frame.info['duration']
+                continue
+            if cum_duration_ms < start_ms + end_ms:
+                frames.append(watermark(frame.convert('RGB')))
+                cum_duration_ms += frame.info['duration']
+            elif len(frames) == 0:
+                # special case: diff is too small that we just have to take a single frame
+                # if end_ms - start_ms < frame.info['frame_duration']:
+                frames.append(watermark(frame.convert('RGB')))
+                break
+            else:
+                break  # early stopping
+        assert len(frames) > 0  # sanity check that there is at least one frame
+        _result: result.Result
+        gif_duration_seconds = target_duration_ms / (1000 * len(frames))  # average frame duration
+        output = BytesIO()
+        frames[0].save(
+            output,
+            format='GIF',
+            save_all=True,
+            append_images=frames[1:],
+            optimize=False,
+            duration=gif_duration_seconds,
+            loop=0
+        )
+        # gif: PIL.GifImagePlugin.GifImageFile = PIL.Image.open(output)
+        return result.Result(
+            media_stream=output,
+            media_type=MediaType.GIF,
+            message=config.message,
+            # gif_duration=gif_duration_seconds,
+        )
