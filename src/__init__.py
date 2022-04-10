9bf404b168a2b69676804b488250c17f4d2ee3a0
Thomas Hesse <mail@thomas-hesse.eu>
2022-04-10 21:28:02 +0200

    fix: src module imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-import client
-import execution
-import model
-import handler
-import timer
-import util
+import src.client as client
+import src.execution as execution
+import src.model as model
+import src.handler as handler
+import src.timer as timer
+import src.util as util
 
 __all__ = [
     client, execution, model, handler, timer, util,
